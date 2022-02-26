2d7679047362ae28f33df8762f1ea6bed25b8cc3
zachj95 <zachariejones@gmail.com>
2022-02-25 20:40:13 -0800

    Solve alt tag issue

--- src/components/LightBox.js
+++ src/components/LightBox.js
@@ -3,7 +3,7 @@ import React from "react";
 function LightBox({ image, setLightBox }) {
   return (
     <div className="lightbox-container" onClick={() => setLightBox("")}>
-      <img src={image} className="lightbox-image" alt="lightbox-image" />
+      <img src={image} className="lightbox-image" alt={image} />
     </div>
   );
 }
